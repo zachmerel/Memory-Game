a99529b54ac8d6f89f46c82b0d8469323fbc5d96
Zach Merel <zachmerel@gmail.com>
2019-03-05 22:44:44 -0600

    card.json file added

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
 
-const Card = (props) => {
+const Card = props => {
   return (
-    <div>
-      <img src={'../images/addison-russell.jpg'} alt="addison russell" />
-    </div>
+    <div
+      className="card"
+      style={{
+        backgroundImage: props.image ? `url(${props.image})` : "none"
+      }}
+    />
   );
 };
 
