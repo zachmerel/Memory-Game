bfe53431f0e80462fe97520877693a86bf9c6693
Zach Merel <zachmerel@gmail.com>
2019-03-15 19:44:08 -0500

    test to see if commit will go thorugh to the correc repo

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,10 +4,7 @@ import "./style.css";
 const Footer = () => {
   return (
     <nav className="navbar fixed-bottom navbar-light bg-light">
-      <div className="navbar-text navbar-element">
-       Memory Game <span><img src={'../../../public/small-localStorage.png'} alt='cubs logo'></img></span>
-      </div>
-      
+      <div className="navbar-text navbar-element">Memory Game</div>
     </nav>
   );
 };
